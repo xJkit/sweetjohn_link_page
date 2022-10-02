618119292d08b11e55e547a4ef7a2f25077cdcad
xJkit <xjkit54780@gmail.com>
2022-10-02 16:41:18 +0800

    新增 GA 到 link page

---
+++ components/GA.js
@@ -0,0 +1,25 @@
+export default function GA() {
+  return (
+    <>
+      <script
+        async
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <script
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+      `,
+        }}
+      ></script>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'config/theme';
 import Head from 'next/head';
+import GA from '../components/GA';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GA />
       <Head>
         <title>甜約翰 Sweet John</title>
         <meta charset="utf-8" />
